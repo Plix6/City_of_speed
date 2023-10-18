a70ff74ad80fb5f6bf118f0772aea711d27fa6c7
Olivier BOSSAVIT <olivier.bossavit@student.junia.com>
2023-10-18 09:38:37 +0200

    initializing leaderboard in data manager

--- Assets/Scripts/DataManagement/DataManagement.cs
+++ Assets/Scripts/DataManagement/DataManagement.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class DataManagement : MonoBehaviour
 {
+    private string fileName = "game.data";
+    private string dirPath = Application.persistentDataPath;
+
     private Timer timer = new Timer();
     private Checkpoint checkpoint = new Checkpoint();
+    private Leaderboard leaderboard;
     // Start is called before the first frame update
     void Start()
     {
-        
+        leaderboard = new Leaderboard(fileName, dirPath);
+        leaderboard.LoadData();
     }
 
     // Update is called once per frame
